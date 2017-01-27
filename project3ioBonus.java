7944d6b8029d368436f1e137e23eea525912a770
Jason Mabanta <jasonmabz1234@gmail.com>
2017-01-27 15:00:33 +0800

    Update project3ioBonus.java

--- project3ioBonus.java
+++ project3ioBonus.java
@@ -3,24 +3,24 @@ import java.io.*;
 class project3ioBonus{
 	public static void main(String[]args){
 		System.out.println("Hello Everyone!");
-		InputStreamReader n = new InputStreamReader(System.in);
-		BufferedReader d = new BufferedReader (n);
+		InputStreamReader j = new InputStreamReader(System.in);
+		BufferedReader m = new BufferedReader (j);
 		System.out.println("Enter Height of the Triangle:");
 		double height =0
 		try{
-			height=Integer.valueof(d.readLine());
+			height=Integer.valueof(m.readLine());
 		}
 		catch(Exception p){
 			System.out.println("Invalid number!")
 			
 		}
-		for(int k=1; k<=height; k++){
-			for(int i=1; i<height -(k-1); i++){
+		for(int g=1; g<=height; g++){
+			for(int h=1; h<height -(g-1);h++){
 				System.out.print(" ");
 			}
-			for(int j=1; j<=k; j++){
+			for(int i=1; i<=g; i++){
 				System.out.print("*");
-				for(int n1=1; n1<j; n1+=j){
+				for(int j1=1; j1<i; j1+=i){
 				System.out.print("*");
 				}
 			}
